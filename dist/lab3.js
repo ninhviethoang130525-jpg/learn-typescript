38457621fa7ec57a0e4faae6d79cdce44f5fbb4e
ninhviethoang130525-jpg <ninhviethoang130525@gmail.com>
2026-09-18 19:55:30 +0700

    lap3

--- dist/lab3.js
+++ dist/lab3.js
@@ -1,2 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const averageScore = (...scores) => {
+    let tong = 0;
+    for (let i = 0; i < scores.length; i++) {
+        tong = tong + scores[i];
+    }
+    return tong / scores.length;
+};
+console.log(averageScore(1, 2, 3, 4, 5));
+console.log(averageScore(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
---
+++ src/lab3.ts
@@ -0,0 +1,10 @@
+const averageScore = (...scores: number[]): number => {
+    let tong = 0;
+    for(let i = 0; i < scores.length; i++){
+        tong = tong + scores[i]!;
+    }
+    return tong / scores.length;
+
+}
+console.log(averageScore(1, 2, 3, 4, 5));
+console.log(averageScore(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
